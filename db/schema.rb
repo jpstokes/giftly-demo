16202c50761356845432520e3f705b715aa3e23f
Jason Stokes <jpstokes@plecco.net>
2018-11-30 09:40:32 -0500

    convert database to use postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_29_055122) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "businesses", force: :cascade do |t|
     t.integer "rating"
     t.string "price"
